0f5b32f5cd7f6cb75da3b431beb2e26a0b5e2434
smhrd <smhrd@DESKTOP-24L66D4>
2024-04-16 16:54:03 +0900

    아현 4번째 완료

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -6,6 +6,9 @@ public class Ex01 {
 		System.out.println("팀장 첫번째 커밋");
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 세번째 커밋");
+		System.out.println("아현: 4번째 커밋");
+		
+		
 	}
 
 }
